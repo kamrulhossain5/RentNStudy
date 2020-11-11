ededc6aa97f4cdd4421a3eb4326bde62c7a8bbd7
Kamrul Hossain <kam.hossain5@gmail.com>
2020-11-11 01:16:15 -0500

    Updated App.test.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../App';
+import App from './App';
 
-test('renders without crashing', () => {
+it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
